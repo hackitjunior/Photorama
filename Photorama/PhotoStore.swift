0a790b8a515c5915c1bfcb8aa2084f68d1abc0c8
Alberto Silva <silvaguzmanalberto@gmail.com>
2021-06-14 10:24:27 -0500

    Enable the download  of images

--- Photorama/PhotoStore.swift
+++ Photorama/PhotoStore.swift
@@ -5,7 +5,13 @@
 //  Created by Alberto Silva on 11/06/21.
 //
 
-import Foundation
+import UIKit
+
+enum PhotoError: Error {
+    case imageCreationError
+    case missingImageURL
+}
+
 class PhotoStore{
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
@@ -18,7 +24,10 @@ class PhotoStore{
         let task = session.dataTask(with: request){
             (data, response, error) in
             let result = self.processPhotosRequest(data: data, error: error)
-            completion(result)
+            
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
         }
         task.resume()
     }
@@ -30,5 +39,36 @@ class PhotoStore{
         }
 
         return FlickrAPI.photos(fromJSON: jsonData)
+    }
+    
+    func fecthImage(for photo: Photo, completion: @escaping (Result<UIImage, Error>) -> Void){
+        guard let photoURL = photo.remoteURL else {
+            completion(.failure(PhotoError.missingImageURL))
+            return
+        }
+        
+        let request = URLRequest(url: photoURL)
+        
+        let task = session.dataTask(with: request){
+            (data, response, error) in
+            let result = self.processImageRequest(data: data, error: error)
+            OperationQueue.main.addOperation {
+                completion(result)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func processImageRequest(data: Data?, error: Error?) -> Result<UIImage, Error>{
+        guard let imageData = data, let image = UIImage(data: imageData) else {
+            if data == nil{
+                return .failure(error!)
+            } else {
+                return .failure(PhotoError.imageCreationError)
+            }
+        }
+        
+        return .success(image)
     }
 }
