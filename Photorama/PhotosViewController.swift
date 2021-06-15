e2f1b0ff0486fad9fb890da5719d19d361b63ebc
Alberto Silva <silvaguzmanalberto@gmail.com>
2021-06-15 10:07:11 -0500

    Allow navigating to a photo

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -36,6 +36,20 @@ class PhotosViewController: UIViewController {
 
         }
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch  segue.identifier {
+        case "showPhoto":
+            if let selectedIndexPath = collectionView.indexPathsForSelectedItems?.first{
+                let photo = photoDataSource.photos[selectedIndexPath.row]
+                let destinationVC = segue.destination as! PhotoInfoViewController
+                destinationVC.photo = photo
+                destinationVC.store = store
+            }
+        default:
+            preconditionFailure("Unexpected segue identifier")
+        }
+    }
 }
 
 extension PhotosViewController: UICollectionViewDelegate {
